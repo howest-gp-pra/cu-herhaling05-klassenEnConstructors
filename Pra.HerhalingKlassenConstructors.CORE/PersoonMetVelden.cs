f2fd7c8cbe6ce9a83f86de43965e0d889e2c578c
jandedeuraerder-howest <jan.de.deurwaerder@howest.be>
2020-09-21 11:36:58 +0200

    Add project files.

---
+++ Pra.HerhalingKlassenConstructors.CORE/PersoonMetVelden.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pra.HerhalingKlassenConstructors.CORE
+{
+    // NIET VERGETEN: je klassen publiek maken !
+    public class PersoonMetVelden
+    {
+        // we werken hier met velden, nog NIET met properties
+        // ter herinnering : velden zijn eigenlijk niets meer dan variabelen die je publiek toegangkelijk maakt
+        // ter herinnering : gebruik je velden, schrijf die dan met een kleine letter
+
+        //=================================
+        // STAP 1  : zonder contstructor
+        //=================================
+
+        public string naam;
+        public string voornaam;
+        public DateTime geboortedatum;
+        public char geslacht;  // afspraak : M = man, V = vrouw, O = onbekend
+
+        //t.b.v. stap 4 
+        private string privaatje;
+
+        //=================================
+        // EINDE STAP 1 
+        //============================================================
+        // STAP 2  : argumentloze contstructor (= default constructor)
+        //============================================================
+
+        public PersoonMetVelden()
+        {
+
+            // vergeet voorlopig even dat onderstaande instructie hier staat
+            // we hebben deze nodig voor STAP 4
+            // deze variabele wordt verder nergens gebruikt
+            privaatje = DateTime.Now.ToShortDateString();
+        }
+
+        // als dit je enige constructor is (dus zonder argumenten en zonder code) dan kan je hem evengoed NIET maken
+        // impliciet is deze immers altijd aanwezig
+
+        //=================================
+        // EINDE STAP 2 
+        //============================================================
+        // STAP 3  : contstructor met 2 argumenten
+        //============================================================
+        // we maken een tweede constructor aan (de eerste, argumentloze, laten we staan)
+        // deze techniek wordt OVERLADEN genoemd en kennen we van onze methoden (zie cursus PRB)
+        // je kan een contructor meerdere keren overladen, zolang het aantal argumenten maar verschillend is
+        // deze versie verwacht de naam en de voornaam
+
+        public PersoonMetVelden(string naam, string voornaam)
+        {
+            // probleem is hier dat het argument met een kleine letter geschreven wordt,
+            // terwijl het veld dat we wensen te bruiken identiek geschreven is.
+            // we lossen dit op door naar het veld te verwijzen met this.
+
+            this.naam = naam;
+            this.voornaam = voornaam;
+        }
+
+        //=================================
+        // EINDE STAP 3 
+        //============================================================
+        // STAP 4  : constructor met 4 argumenten
+        //============================================================
+        // we maken nu een derde constructor aan met 4 argumenten
+        // VOORALEER WE DIT DOEN VOEGEN WE BOVENAAN NOG EEN EXTRA VELD TOE : het veld privaatje (zie code bovenaan)
+        // je hebt er op gelet dat deze variabele een waarde krijgt in de argumentloze constructor
+        // willen we hebben dat deze code ook wordt uitgevoerd bij gebruik van een andere constructor,
+        // dan moeten we deze uitbreiden met   :this()  .  Dit heet constructor-chaining
+        public PersoonMetVelden(string naam, string voornaam, DateTime geboortedatum, char geslacht) : this()
+        {
+            this.naam = naam;
+            this.voornaam = voornaam;
+            this.geboortedatum = geboortedatum;
+            this.geslacht = geslacht;
+        }
+
+        // deze constructor chaining moeten we eigenlijk ook bij onze constructor met 2 argumenten implementeren
+        // zodat ook hier de instructie uit de argumentloze contructor uitgevoerd worden
+
+    }
+}
---
+++ Pra.HerhalingKlassenConstructors.WPF/MainWindow.xaml.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Pra.HerhalingKlassenConstructors.CORE;
+
+namespace Pra.HerhalingKlassenConstructors.WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnPMV1_Click(object sender, RoutedEventArgs e)
+        {
+            // BEKIJK DE KLASSE PERSOONMETVELDEN : deze code gebruikt STAP 1, situatie waarbij geen constructor bestaat
+            // ps : in vorige voorbeeld (herhaling.klassen) hebben we uitsluitend met deze situatie gewerkt
+
+            lstDisplay.Items.Clear();
+
+            PersoonMetVelden persoon = new PersoonMetVelden();
+            persoon.naam = "Piraat";
+            persoon.voornaam = "Piet";
+            persoon.geboortedatum = DateTime.Parse("1987-05-30");
+            persoon.geslacht = 'M';
+            lstDisplay.Items.Add(persoon.voornaam + " " + persoon.naam + " (geslacht = " + persoon.geslacht + ")");
+
+            persoon = new PersoonMetVelden();
+            persoon.naam = "Taar";
+            persoon.voornaam = "Guy";
+            persoon.geboortedatum = DateTime.Parse("1961-11-03");
+            persoon.geslacht = 'M';
+            lstDisplay.Items.Add(persoon.voornaam + " " + persoon.naam + " (geslacht = " + persoon.geslacht + ")");
+
+            persoon = new PersoonMetVelden();
+            persoon.naam = "Ba";
+            persoon.voornaam = "Sam";
+            persoon.geboortedatum = DateTime.Parse("1012-01-03");
+            persoon.geslacht = 'X';
+            lstDisplay.Items.Add($"{persoon.voornaam} {persoon.naam} (geslacht = {persoon.geslacht} )");  // alternatieve schrijfwijze
+
+        }
+        private void BtnPMV2_Click(object sender, RoutedEventArgs e)
+        {
+            // BEKIJK DE KLASSE PERSOONMETVELDEN : deze code gebruikt STAP 2, situatie waarbij 
+            // een lege constructor gebruikt wordt.  Dit is een constructor die geen argumenten ontvangt
+            // en die geen code bevat.
+            // Is dit de enige constructor in je klasse dan kan je die evengoed weglaten.
+            // de code hieronder is dus identiek aan de eventhandler hierboven
+
+            lstDisplay.Items.Clear();
+
+            PersoonMetVelden persoon = new PersoonMetVelden();
+            persoon.naam = "Piraat";
+            persoon.voornaam = "Piet";
+            persoon.geboortedatum = DateTime.Parse("1987-05-30");
+            persoon.geslacht = 'M';
+            lstDisplay.Items.Add(persoon.voornaam + " " + persoon.naam + " (geslacht = " + persoon.geslacht + ")");
+
+            persoon = new PersoonMetVelden();
+            persoon.naam = "Taar";
+            persoon.voornaam = "Guy";
+            persoon.geboortedatum = DateTime.Parse("1961-11-03");
+            persoon.geslacht = 'M';
+            lstDisplay.Items.Add(persoon.voornaam + " " + persoon.naam + " (geslacht = " + persoon.geslacht + ")");
+
+            persoon = new PersoonMetVelden();
+            persoon.naam = "Ba";
+            persoon.voornaam = "Sam";
+            persoon.geboortedatum = DateTime.Parse("1012-01-03");
+            persoon.geslacht = 'X';
+            lstDisplay.Items.Add($"{persoon.voornaam} {persoon.naam} (geslacht = {persoon.geslacht} )");  // alternatieve schrijfwijze
+
+        }
+
+        private void BtnPMV3_Click(object sender, RoutedEventArgs e)
+        {
+            // BEKIJK DE KLASSE PERSOONMETVELDEN : deze code gebruikt STAP 3, situatie waarbij 
+            // een contructor werd bijgemaakt die nu 2 argumenten gebruikt.
+            // Wanneer we nu een object aanmaken hebben we nu de mogelijkheid om te kiezen welke contructor 
+            // we willen gebruiken.
+            // in de code hieronder gaan we het eerste object aanmaken m.b.v. de argumentloze contructor, 
+            // het tweede en derde object m.b.v. de constructor die 2 argumenten verwacht
+
+            lstDisplay.Items.Clear();
+
+            PersoonMetVelden persoon = new PersoonMetVelden();
+            persoon.naam = "Piraat";
+            persoon.voornaam = "Piet";
+            persoon.geboortedatum = DateTime.Parse("1987-05-30");
+            persoon.geslacht = 'M';
+            lstDisplay.Items.Add(persoon.voornaam + " " + persoon.naam + " (geslacht = " + persoon.geslacht + ")");
+
+            persoon = new PersoonMetVelden("Taar", "Guy");
+            persoon.geboortedatum = DateTime.Parse("1961-11-03");
+            persoon.geslacht = 'M';
+            lstDisplay.Items.Add(persoon.voornaam + " " + persoon.naam + " (geslacht = " + persoon.geslacht + ")");
+
+            persoon = new PersoonMetVelden("Ba", "Sam");
+            persoon.geboortedatum = DateTime.Parse("1012-01-03");
+            persoon.geslacht = 'X';
+            lstDisplay.Items.Add($"{persoon.voornaam} {persoon.naam} (geslacht = {persoon.geslacht} )");  // alternatieve schrijfwijze
+
+
+
+        }
+
+        private void BtnPMV4_Click(object sender, RoutedEventArgs e)
+        {
+            // BEKIJK DE KLASSE PERSOONMETVELDEN : deze code gebruikt STAP 4, situatie waarbij 
+            // een contructor werd bijgemaakt die nu 4 argumenten gebruikt.
+            // Wanneer we nu een object aanmaken hebben we keuze uit 3 constructors
+            // in de code hieronder gaan we het eerste object aanmaken m.b.v. de argumentloze contructor, 
+            // het tweede m.b.v. de constructor die 2 argumenten verwacht en
+            // het derde m.b.v. de constructor die 3 argumenten verwacht
+            // bekijk in je klasse zelf ook de opmerking rond constructor-chaining
+
+            lstDisplay.Items.Clear();
+
+            PersoonMetVelden persoon = new PersoonMetVelden();
+            persoon.naam = "Piraat";
+            persoon.voornaam = "Piet";
+            persoon.geboortedatum = DateTime.Parse("1987-05-30");
+            persoon.geslacht = 'M';
+            lstDisplay.Items.Add(persoon.voornaam + " " + persoon.naam + " (geslacht = " + persoon.geslacht + ")");
+
+            persoon = new PersoonMetVelden("Taar", "Guy");
+            persoon.geboortedatum = DateTime.Parse("1961-11-03");
+            persoon.geslacht = 'M';
+            lstDisplay.Items.Add(persoon.voornaam + " " + persoon.naam + " (geslacht = " + persoon.geslacht + ")");
+
+            persoon = new PersoonMetVelden("Ba", "Sam", DateTime.Parse("1012-01-03"), 'X');
+            lstDisplay.Items.Add($"{persoon.voornaam} {persoon.naam} (geslacht = {persoon.geslacht} )");  // alternatieve schrijfwijze
+
+            // bekijk de hoeveelheid code die je nodig hebt om het 1°,het 2° en het 3° object aan te maken
+            // je merkt dat een "uitgebreide" constructor je achteraf toch wel wat typewerk kan besparen.
+            // maar dat is uiteraard niet de enige bedoeling van constructors.
+            // we komen hier in een volgend voorbeeld nog op terug
+        }
+    }
+}
